2478e8e8567bd0b4dc78542d073da909ee8493c0
Peter Jang <peter.x.jang@gmail.com>
2020-03-01 10:00:07 -0600

    Add time route/controller response action

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -2,4 +2,8 @@ class Api::ExamplePagesController < ApplicationController
   def hello_method
     render json: { message: "hello!!!" }
   end
+
+  def time_method
+    render json: { message: "Welcome to the time app!", time: Time.now.strftime("%A, %b %d") }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/hello_url" => "example_pages#hello_method"
+    get "/time_url" => "example_pages#time_method"
   end
 end
